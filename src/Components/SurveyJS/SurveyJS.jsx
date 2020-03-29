db9e22fa0f661e6baf1e504b6e2212860a1668af
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2020-03-29 12:12:19 -0500

    Use componetns

--- src/Components/SurveyJS/SurveyJS.jsx
+++ src/Components/SurveyJS/SurveyJS.jsx
@@ -1,98 +1,33 @@
-//In your react App.js or yourComponent.js file add these lines to import
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import * as Survey from "survey-react";
-import "survey-react/survey.css";
+import * as Survey from 'survey-react';
+import 'survey-react/modern.css';
+
+import { mockQuestionnaire } from '__mocks__/mockQuestionnaire';
+
+// apply theme modern
+Survey.StylesManager.applyTheme('modern');
 
 class SurveyJS extends Component {
-    //Define Survey JSON
-    //Here is the simplest Survey with one text question
-    json = {
-        "title": "Triage 1",
-        "showProgressBar": "bottom",
-        "showTimerPanel": "top",
-        "firstPageIsStarted": false,
-        "startSurveyText": "Evaluar condicion",
-        "pages": [
-            {
-                "questions": [
-                    {
-                        "type": "radiogroup",
-                        "name": "question-1",
-                        "title": "¿Cuál es su temperatura actual?",
-                        "choices": [
-                            "Si",
-                            "No"
-                        ]
-                    }
-                ]
-            },
-            {
-                "questions": [
-                    {
-                        "type": "radiogroup",
-                        "name": "question-2",
-                        "title": "¿Ha tenido contacto con personas contagiadas con COVID-19?",
-                        "choices": [
-                            "Si",
-                            "No"
-                        ]
-                    }
-                ]
-            },
-            {
-                "questions": [
-                    {
-                        "type": "radiogroup",
-                        "name": "question-3",
-                        "title": "¿Tienes tos seca?",
-                        "choices": [
-                            "Si",
-                            "No"
-                        ]
-                    }
-                ]
-            },
-            {
-                "questions": [
-                    {
-                        "type": "radiogroup",
-                        "name": "question-4",
-                        "title": "¿Tienes dificultades olfatorias?",
-                        "choices": [
-                            "Si",
-                            "No"
-                        ]
-                    }
-                ]
-            }
-        ],
-        "completedHtml": "<h4>Gracias por sus respuestas..</h4>"
-    };
-
-    //Define a callback methods on survey complete
-    onComplete(survey, options) {
-        //Write survey results into database
-        console.log("Survey results: " + JSON.stringify(survey.data));
-    }
-    render() {
-        //Create the model and pass it into react Survey component
-        //You may create survey model outside the render function and use it in your App or component
-        //The most model properties are reactive, on their change the component will change UI when needed.
-        var model = new Survey.Model(this.json);
-        return <Survey.Survey model={model} onComplete={this.onComplete} />;
-        /*
-  //The alternative way. react Survey component will create survey model internally
-  return (<Survey.Survey json={this.json} onComplete={this.onComplete}/>);
-  */
-        //You may pass model properties directly into component or set it into model
-        // <Survey.Survey model={model} mode="display"/>
-        //or
-        // model.mode="display"
-        // <Survey.Survey model={model}/>
-        // You may change model properties outside render function.
-        //If needed react Survey Component will change its behavior and change UI.
-    }
+  //The most model properties are reactive, on their change the component will change UI when needed.
+  model = new Survey.Model(mockQuestionnaire);
+  /**
+   * is fire when the user finish the survey
+   * @param {*} survey all model with data
+   * @param {*} options function to show different messages
+   */
+  onComplete(survey, options) {
+    console.log('Survey results:', { survey, options, data: survey.data });
+  }
+  render() {
+    return (
+      <Survey.Survey
+        model={this.model}
+        onComplete={this.onComplete}
+        locale="es"
+      />
+    );
+  }
 }
 
-export default SurveyJS
+export default SurveyJS;
---
+++ src/__mocks__/mockQuestionnaire.js
@@ -0,0 +1,50 @@
+export const mockQuestionnaire = {
+  title: 'Triage 1',
+  showProgressBar: 'bottom',
+  showTimerPanel: false,
+  firstPageIsStarted: false,
+  startSurveyText: 'Evaluar condicion',
+  pages: [
+    {
+      questions: [
+        {
+          type: 'radiogroup',
+          name: 'question-1',
+          title: '¿Cuál es su temperatura actual?',
+          choices: ['Si', 'No'],
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          type: 'radiogroup',
+          name: 'question-2',
+          title: '¿Ha tenido contacto con personas contagiadas con COVID-19?',
+          choices: ['Si', 'No'],
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          type: 'radiogroup',
+          name: 'question-3',
+          title: '¿Tienes tos seca?',
+          choices: ['Si', 'No'],
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          type: 'radiogroup',
+          name: 'question-4',
+          title: '¿Tienes dificultades olfatorias?',
+          choices: ['Si', 'No'],
+        },
+      ],
+    },
+  ],
+  completedHtml: '<h4>Gracias por sus respuestas..</h4>',
+};
